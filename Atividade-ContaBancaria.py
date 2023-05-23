1eb58617dc2b9f1790de581fce366248fa8ab193
brensmiranda <brendah.mirandaa@hotmail.com>
2023-05-23 20:36:27 -0300

    Atividade Conta Bancaria. :boom:

--- Atividade-ContaBancaria.py
+++ Atividade-ContaBancaria.py
@@ -2,46 +2,51 @@
 class Banco:
     soma = 0
     subtracao = 0
-    ativar = "sim"
+    desativar = "sim"
 
-    def __init__(self, nome, tipoConta, numConta, numAgencia, saldo):
+    def __init__(self, nome, tipoConta, numConta, numAgencia, saldo, statusConta=True):
         self.nome = nome
         self.tipoConta = tipoConta
         self.numConta = numConta
         self.numAgencia = numAgencia
         self.saldo = saldo
-
+        self.statusConta = statusConta
 
     def deposito(self):
-        valorDeposito = float(input("Quanto você quer depositar?"))
+        valorDeposito = float(input("Quanto você quer depositar? "))
 
         if valorDeposito >= 0:
             self.saldo += valorDeposito
             self.soma += valorDeposito
+
     def transferencia(self):
-        transferencia = float(input("Quanto quer transferir?"))
+        valorTransferencia = float(input("Quanto você quer transferir? "))
 
-        if transferencia >=0:
-            self.saldo -= transferencia
-            self.subtracao -= transferencia
-    def ativarConta(self):
-        ativarConta = input("Você deseja ativar a conta? Digite sim ou não.").lower()
-        if ativarConta==self.ativar:
-            print('Conta ativa.' )
+        if valorTransferencia >= 0 and self.saldo >= valorTransferencia:
+            self.saldo -= valorTransferencia
+            self.subtracao += valorTransferencia
         else:
-            print('Conta desativada.')
+            print("Saldo insuficiente para transferência.")
 
-
-
+    def desativarConta(self):
+        desativarConta = input("Você deseja desativar a conta? Digite sim ou não: ").lower()
+        if self.saldo == 0:
+            print("A conta pode ser desativada.")
+        elif self.statusConta and desativarConta == self.desativar:
+            self.statusConta = False
+            print("Conta desativada.")
+        else:
+            print("Conta continua ativada.")
 
     def __str__(self):
         return f"Nome: {self.nome}\nTipo de Conta: {self.tipoConta}\nNúmero da Conta: {self.numConta}\nNúmero da Agência: {self.numAgencia}\nSaldo: {self.saldo}"
 
+
 conta1 = Banco("brenda", "corrente", "3422125-3", "00", 10.00)
-print("saldo:", conta1.saldo)
+print("Saldo:", conta1.saldo)
 conta1.deposito()
 conta1.transferencia()
-conta1.ativarConta()
+conta1.desativarConta()
 
 print(conta1)
 
